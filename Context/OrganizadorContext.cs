1537b0c4798d780611fa098f923d3eb56a8378af
jelainemartins <jcm210891@gmail.com>
2024-05-17 13:47:24 -0300

    Adicionar arquivos de projeto.

---
+++ Context/OrganizadorContext.cs
@@ -0,0 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using TrilhaApiDesafio.Models;
+
+namespace TrilhaApiDesafio.Context
+{
+    public class OrganizadorContext : DbContext
+    {
+        public OrganizadorContext(DbContextOptions<OrganizadorContext> options) : base(options)
+        {
+            
+        }
+        public DbSet<Tarefa> Tarefas { get; set; }
+    }
+}
